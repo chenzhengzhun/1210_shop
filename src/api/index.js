825b706760f5725d597ebc352c7256b9182f848a
xiaopi <153387143@qq.com>
2019-12-10 18:14:57 +0800

    init shop

---
+++ src/api/index.js
@@ -0,0 +1,23 @@
+/*
+包含n个接口请求的模块
+*/
+import ajax from './ajax'
+// [1、根据经纬度获取位置详情]
+export const reqAdderss = (geohash) => ajax(`/posititon/${geohash}`)
+// [2、获取食品分类列表]
+export const reqFoodTypes = () => ajax(`index_category`)
+// [3、根据经纬度获取商铺列表]
+export const reqShopList = (latitude,longitude) => ajax(`/shops`,{latitude,longitude})
+// [4、根据经纬度和关键字搜索商铺列表]
+export const reqSearch = (geohash,keyword) => ajax(`/search_shops`,{geohash,keyword})
+// [5、获取一次性验证码]
+// [6、用户名密码登陆]
+export const reqLogin = (name,pwd,captcha) => ajax(`/login_pwd`,{name,pwd,captcha})
+// [7、发送短信验证码]
+export const reqPhoneNumber = (phone) => ajax(`/sendcode`,phone)
+// [8、手机号验证码登陆]
+
+// [9、根据会话获取用户信息]
+export const reqUserinfo = () => ajax(`/userinfo`)
+// [10、用户登出]
+export const reqLogout = () => ajax(`/logout`)
